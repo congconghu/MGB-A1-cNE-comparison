1cd6bc211fe9a69f608c7c3175e69d32fb87efe7
congconghu <congconghu96@gmail.com>
2022-12-05 17:02:27 -0800

    pickle single units

--- convert_files.py
+++ convert_files.py
@@ -1,12 +1,31 @@
-import data_from_mat_to_pkl as mtp
-import helper
 import os
+import glob
 import pickle
 import re
+import mat73
+
+import data_from_mat_to_pkl as mtp
+import helper
+
+# ------------------------------------------- pickle single units-------------------------------------------------------
+datafolder = r'E:\Congcong\Documents\data\comparison\data-Matlab'
+files = glob.glob(datafolder + r'\*split.mat', recursive=False)
+
+for idx, file in enumerate(files):
+    print('({}/{})Save pickle file:'.format(idx+1, len(files)), file)
+    session = mtp.Session()
+    session.read_mat_file(file)
+    file_pkl = re.sub('-spk-curated-split.mat', '.pkl', file)
+    file_pkl = re.sub('Matlab', 'pkl', file_pkl)
+    session.save_pkl_file(file_pkl)
+
+# load pickle file
+with open(file_pkl, 'rb') as f:
+    session = pickle.load(f)
 
 # ------------------------------------------- pickle stimulus----------------------------------------------------------
 stimfolder = r'E:\Congcong\Documents\stimulus\thalamus'
-stimfile = r'rn1-500flo-40000fhi-0-4SM-0-40TM-40db-96khz-48DF-15min-seed190506_stim.mat'
+stimfile = r'rn1-500flo-40000fhi-0-4SM-0-40TM-40db-96khz-48DF-15min-seed190506_DFt1_DFf5_stim.mat'
 stimfile_pkl = re.sub('_stim.mat', '.pkl', stimfile)
 
 stimfile_path = os.path.join(stimfolder, stimfile)
@@ -25,4 +44,4 @@ with helper.Timer():
     with open(stimfile_pkl_path, 'rb') as f:
         stim = pickle.load(f)
 
-# ------------------------------------------- pickle single units-------------------------------------------------------
+
--- data_from_mat_to_pkl.py
+++ data_from_mat_to_pkl.py
@@ -11,12 +11,13 @@ class Stimulus:
     def __init__(self, stim_mat=None, taxis=None, faxis=None, phase=None, fs=None, df=None, app=None, amp_dist=None,
                  max_fm=None, max_rd=None):
         """Initiate empty object of Stimulus (can be later modified to create Stimulus from scratch)
-        Parameters of stimulus:
+
+        INPUT:
         stim_mat: nf x nt spectrogram of stimulus
         taxis: time axis for a block of stimulus
         faxis: frequency axis of the prectrogram
         phase: phase at each time bin for the spectrogram
-        fs: sound file sampling rate, in kHz
+        fs: sound file sampling rate, in Hz
         df: down sample rate to get stim_mat from the sound file
         app: modulation amplitude
         amp_dist: unit of modulation amplitude
@@ -37,18 +38,15 @@ class Stimulus:
     def read_mat_file(self, stimfile_path):
         """Read .mat files for the stimulus and save parameters in Stimulus class"""
         # load stimulus file
-        try:
-            loadmat(stimfile_path)
-        except NotImplementedError:
-            data_dict = mat73.loadmat(stimfile_path)
-            self.stim_mat = data_dict['stim_mat']
-
+        data_dict = mat73.loadmat(stimfile_path)
+        self.stim_mat = data_dict['stim_mat']
+        # load stimulus parameters
         paramfile_path = re.sub('_stim', '_param', stimfile_path)
         params = loadmat(paramfile_path)
         self.taxis = params['taxis'][0]
         self.faxis = params['faxis'][0]
         self.phase = params['phase'][0]
-        self.fs = params['Fs'][0][0]
+        self.fs = params['Fs'][0][0] * 1e3
         self.DF = params['DF'][0][0]
         self.App = params['App'][0][0]
         self.AmpDist = params['AmpDist'][0]
@@ -60,3 +58,70 @@ class Stimulus:
             pickle.dump(self, output, pickle.HIGHEST_PROTOCOL)
 
 
+class SingleUnit:
+
+    def __init__(self, unit, chan, spiketimes, isolation, peak_snr, isi_bin, isi_vio,
+                 waveforms=None, waveforms_mean=None, waveforms_std=None, amps=None,
+                 adjacent_chan=None, template=None):
+        self.unit = unit
+        self.chan = chan
+        self.spiketimes = spiketimes
+        self.isolation = isolation
+        self.peak_snr = peak_snr
+        self.isi_bin = isi_bin
+        self.isi_vio = isi_vio
+        self.waveforms = waveforms
+        self.waveforms_mean = waveforms_mean
+        self.waveforms_std = waveforms_std
+        self.amps = amps
+        self.adjacent_chan = adjacent_chan
+        self.template = template
+
+
+class Session:
+
+    def __init__(self, exp=None, site=None, depth=None, filt_params=None, fs=None, probe=None, probdata=None, units=[]):
+        """Initiate empty object of Stimulus (can be later modified to create Stimulus from scratch)
+
+        INPUT:
+        exp: time of recording in the format of "YYMMDD_HHMMSS"
+        site: recording site
+        depth: depth of the tip of probe
+        filt_params: filter parameter for automatic curation
+        fs: recording sampling rate in Hz
+        probe: type of probe used for recording, 'H31x64' or 'H22x32'
+        probdata: layout of probe
+        """
+        self.exp = exp
+        self.site = site
+        self.depth = depth
+        self.filt_params = filt_params
+        self.fs = fs
+        self.probe = probe
+        self.probdata = probdata
+        self.units = units
+
+    def read_mat_file(self, datafile_path):
+        """Read .mat files of the recording and convert data to Session object"""
+        # load stimulus file
+        data_dict = mat73.loadmat(datafile_path)
+        self.exp = data_dict['spk']['exp']
+        self.site = int(data_dict['spk']['site'])
+        self.depth = int(data_dict['spk']['depth'])
+        self.filt_params = data_dict['spk']['filt_params']
+        self.fs = int(data_dict['spk']['fs'])
+        self.probe = data_dict['spk']['probe']
+        self.probdata = data_dict['spk']['probdata']
+        self.units = []
+        for unit in data_dict['spk']['spk']:
+            su = SingleUnit(unit=int(unit['unit']), chan=int(unit['chan']), spiketimes=unit['spiketimes'],
+                            isolation=float(unit['isolation']), peak_snr=unit['peak_snr'],
+                            isi_bin=float(unit['isibin']), isi_vio=float(unit['isi_vio']),
+                            waveforms=unit['waveforms'], waveforms_mean=unit['waveforms_mean'],
+                            waveforms_std=unit['waveforms_std'], amps=unit['amps'], adjacent_chan=unit['adjacent_chan'],
+                            template=unit['template'])
+            self.units.append(su)
+
+    def save_pkl_file(self, savefile_path):
+        with open(savefile_path, 'wb') as output:
+            pickle.dump(self, output, pickle.HIGHEST_PROTOCOL)
